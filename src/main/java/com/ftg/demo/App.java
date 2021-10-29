64c85ac13c9e72d834bdf85120ac1301eb4f868f
ykong <ykonghao@icloud.com>
2021-10-29 13:56:44 +0800

    Update

--- src/main/java/com/ftg/demo/App.java
+++ src/main/java/com/ftg/demo/App.java
@@ -1,12 +1,17 @@
 package com.ftg.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+        LOGGER.info("服务启动成功！温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
     }
 }
