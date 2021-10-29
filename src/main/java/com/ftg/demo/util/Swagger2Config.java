ebe56edf0c30be2580d3fbd660dd82d1b765b30e
ykong <ykonghao@icloud.com>
2021-10-29 14:56:46 +0800

    Update

--- src/main/java/com/ftg/demo/util/Swagger2Config.java
+++ src/main/java/com/ftg/demo/util/Swagger2Config.java
@@ -13,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
     @Bean
     public Docket getUserDocket(){
         ApiInfo apiInfo=new ApiInfoBuilder()
